86339360a1a99f8e15b6b1eaa0027f5e23ddf9b3
Dariyanto <dariyan007@gmail.com>
2024-05-22 13:47:56 +0700

    Add CORS to getData.php

--- getData.php
+++ getData.php
@@ -28,5 +28,18 @@
 		$data["CPU"][] = array(trim($key), trim($value));
 	}
 
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	        header('Access-Control-Allow-Origin: *');
+	        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+	        header('Access-Control-Allow-Headers: token, Content-Type');
+	        header('Access-Control-Max-Age: 1728000');
+	        header('Content-Length: 0');
+	        header('Content-Type: text/plain');
+	        die();
+	    }
+	
+	    header('Access-Control-Allow-Origin: *');
+	    header('Content-Type: application/json');
+
 	echo json_encode($data);
 ?>
